14f402b0dc4eb8e0f51b1a1888fca8cc427c7aba
GummyBearStudio <keyboardwarriror@gmail.com>
2019-02-28 19:20:20 +0100

    Slide class extended

--- GoogleHashCode/Slide.cs
+++ GoogleHashCode/Slide.cs
@@ -6,6 +6,15 @@ namespace GoogleHashCode
     public class Slide
     {
         public List<Image> elems {get; set;}
+        public List<string> Tags {get {
+            var tmp = new List<string>();
+            foreach (var e in elems)
+                foreach (var t in e.Tags)
+                    if (!tmp.Contains(t))
+                        tmp.Add(t);
+            return tmp;
+        }}
+        public int AmountOfTags { get {return Tags.Count;} }
 
         public Slide(List<Image> imglist)
         {
